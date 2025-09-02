7b95a78c4a31fc014d8ca11bf5228408f56263ca
Gioele Cantoni <gioele.cantoni@gmail.com>
2025-09-02 21:36:11 +0200

    Add .gitignore and enhance build script for SQLite-WASM setup

--- build.sh
+++ build.sh
@@ -14,6 +14,8 @@ setup() {
     echo "$CPUS"
 }
 
+git clean -fdX
+
 (cd modules/sqlite && git reset --hard HEAD && git clean -fd)
 ./modules/emsdk/emsdk install latest
 ./modules/emsdk/emsdk activate latest
@@ -29,4 +31,19 @@ do
 done
 
 (cd modules/sqlite/ext/wasm && make -j$(setup) dist sqlite3_wasm_extra_init.c=../../../../wasm.c)
-#mv modules/sqlite/ext/wasm/sqlite-wasm-*.zip $(TARGET)
+unzip modules/sqlite/ext/wasm/sqlite-wasm-*.zip -d tmp
+mkdir -p sqlite-wasm/sqlite-wasm/jswasm
+mv tmp/sqlite-wasm-*/jswasm sqlite-wasm/sqlite-wasm/.
+rm -rf tmp
+
+cp modules/sqlite-wasm/.prettierrc sqlite-wasm/.
+cp modules/sqlite-wasm/index.d.ts sqlite-wasm/.
+cp modules/sqlite-wasm/index.mjs sqlite-wasm/.
+cp modules/sqlite-wasm/node.mjs sqlite-wasm/.
+cp modules/sqlite-wasm/tsconfig.json sqlite-wasm/.
+
+PKG=sqlite-wasm/package.json
+TMP=sqlite-wasm/package.tmp.json
+
+jq --arg version "$(cat modules/sqlite/VERSION)-sync.$(cd modules/sqlite-sync && make version)-vector.$(cd modules/sqlite-vector && make version)" '.version = $version' "$PKG" > "$TMP" && mv "$TMP" "$PKG"
+(cd sqlite-wasm && npm i && npm run fix && npm run publint && npm run check-types)
