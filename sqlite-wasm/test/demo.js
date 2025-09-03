18bec6a76b66983abdc40959fa75c145b6edf74c
Gioele Cantoni <gioele.cantoni@gmail.com>
2025-09-03 20:20:39 +0200

    Enhance testing setup by adding demo HTML and JS files, and update package.json scripts for automated testing

---
+++ sqlite-wasm/test/demo.js
@@ -0,0 +1,63 @@
+'use strict';
+(function(){
+  const logHtml = function(cssClass,...args){
+    postMessage({
+      type:'log',
+      payload:{cssClass, args}
+    });
+  }
+  const log = (...args)=>logHtml('',...args);
+  const error = (...args)=>logHtml('error',...args);
+
+  const testVersion = function(sqlite3){
+    const db = new sqlite3.oo1.DB("/mydb.sqlite3",'ct');
+
+    try {
+      let testString = '';
+      db.exec({
+        sql: "select sqlite_version();",
+        rowMode: 'array',
+        callback: function(version){
+          testString += version;
+        }
+      });
+      db.exec({
+        sql: "select cloudsync_version();",
+        rowMode: 'array',
+        callback: function(version){
+          testString += `-sync.${version}`;
+        }
+      });
+      db.exec({
+        sql: "select vector_version();",
+        rowMode: 'array',
+        callback: function(version){
+          testString += `-vector.${version}`;
+        }
+      });
+      log(testString);
+    }finally{
+      db.close();
+    }
+
+  };
+
+  if(globalThis.window!==globalThis) {
+    let sqlite3Js = 'sqlite3.js';
+    const urlParams = new URL(globalThis.location.href).searchParams;
+    if(urlParams.has('sqlite3.dir')){
+      sqlite3Js = urlParams.get('sqlite3.dir') + '/' + sqlite3Js;
+    }
+    importScripts(sqlite3Js);
+  }
+  globalThis.sqlite3InitModule({
+    print: log,
+    printErr: console.error
+  }).then(function(sqlite3){
+    try {
+      testVersion(sqlite3);
+    }catch(e){
+      error("Exception:",e.message);
+    }
+  });
+})();
---
+++ sqlite-wasm/test/test.cjs
@@ -0,0 +1,26 @@
+const { chromium, firefox, webkit } = require('playwright');
+const packageJson = require('../package.json');
+
+(async () => {
+    [
+        chromium,
+        firefox,
+        webkit
+    ].forEach(async (browser) => {
+        const window = await browser.launch();
+        const context = await window.newContext();
+        const page = await context.newPage();
+
+        await page.goto('http://127.0.0.1:8081/test/demo.html');
+
+        //sleep 5s
+        await new Promise(r => setTimeout(r, 5000));
+
+        const version = await page.evaluate(() => document.body.innerText);
+
+        if (version != packageJson.version) throw Error('Error: version mismatch');
+        console.log(`✅ ${browser.name()} sqlite-wasm test passed`);
+        
+        await window.close();
+    });
+})();
